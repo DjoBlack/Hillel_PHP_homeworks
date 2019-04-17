842cc57488a72752b915eb16449f70234f0ecf68
Vladimir <djonblack.mail@gmail.com>
2019-04-17 23:13:05 +0300

    Delete post.php

--- post.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	require_once './models/post_model.php';
-	$postModel = new PostModel();
-	$post = $postModel->getPostById($_GET['id']);
-	require_once 'view/post_view.php';
